711498bdae60dea4c63204084b526a2d0f6acee7
pavva91 <valerio.mattioli@protonmail.com>
2023-09-09 17:31:05 +0200

    Custom Handlers implement ServeHTTP method of http.Handler interface

--- main.go
+++ main.go
@@ -10,6 +10,17 @@ func main() {
 	const port = "8081"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	// NOTE: handler with named or anonym function are the same
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	mux.HandleFunc("/healthz2", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK2"))
+	})
+
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
@@ -17,11 +28,17 @@ func main() {
 		Handler: corsMux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	// mux.Handle("/app/", http.FileServer(http.Dir(".")))
+	// mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func middlewareCors(next http.Handler) http.Handler {
